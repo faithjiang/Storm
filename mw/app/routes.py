c50715c8bf0a791308cdf2691bed1a1c89bddee6
Edward Han <ehan1990@gmail.com>
2014-07-06 22:41:41 -0700

    added simple post/get

--- mw/app/routes.py
+++ mw/app/routes.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
- 
+from flask import Flask, render_template, request
+import json
+
 app = Flask(__name__)      
  
 @app.route('/')
 def home():
-  return "hiii"
- 
+    return "hi"
+
+@app.route('/write', methods=['POST'])
+def write():
+    res = request.get_json(force=True)
+    print(res)
+    return "success"
+
+@app.route('/receive')
+def receive():
+    message = {
+        'page': "receive",
+        'message': "receive get"
+    }
+    jsonMessage = json.dumps(message)
+    return jsonMessage
+
 if __name__ == '__main__':
   app.run(debug=True)
