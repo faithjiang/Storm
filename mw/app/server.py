b15da78d1dd5c7c150a63d1213109c0ada32856a
Edward Han <ehan1990@gmail.com>
2014-07-22 00:30:03 -0700

    added form requests

--- mw/app/server.py
+++ mw/app/server.py
@@ -6,10 +6,24 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/summary')
+@app.route('/summary', methods=['GET','POST'])
 def summaryPage():
-    print("called summary")
-    return render_template("tutorial/summary.html")
+    app.logger.info("called summary api")
+    message = None
+    if request.method == "POST":
+        message = {
+            'firstname': request.form['firstname'],
+            'lastname': request.form['lastname'],
+            'year': request.form['year'],
+            'department': request.form['department'],
+            'course1': request.form['course1'],
+            'course2': request.form['course2'],
+            'course3': request.form['course3'],
+        }
+        app.logger.info(message)
+        return render_template("tutorial/summary.html", message=message)
+    else:
+        return render_template("tutorial/summary.html", message=message)
 
 @app.route('/')
 def homePage():
